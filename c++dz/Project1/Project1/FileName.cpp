1c83a640ddcc9e5864f62fe279f71f6f330177ba
notrakd <dmitrytroyt@gmail.com>
2025-09-13 01:21:34 +0300

    Add files via upload

---
+++ c++dz/Project1/Project1/FileName.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <locale>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	//cout << "Блукає осінь. \"Безгомінням\" \nцвіте її очей блакить, \nі у садах під вітром синім \nбагняний плащ її шумить.";
+	cout << "Autumn wanders. \nWith a silence\nthe blue of her eyes blooms, \nand in the gardens under the blue wind \nher marshy cloak rustles.";
+
+
+
+
+
+	//cout << "\т\/Вона мете сумні алеї,\/ \nде пада золото руде, \n\\і важкогривий кінь за нею\\ \nна чорнім поводі іде. \n(Володимир Сосюра. \tБлукає осінь.Безгомінням...)";
+	cout << "\n\/She sweeps the sad alleys,\/ \nwhere red gold falls, \n\\and a heavy-maned horse follows her\\ \non a black rein. \n(Vladimir Saussure. \tAutumn wanders. In silence...)";
+
+	/*
+	* сделал задание но не смог не каким образом подключить укр язык, пробувал гуглить и найти как можно ещё его подключить но невышло, поэтоэтому
+	* перевёл на англ. и сделал с ним что б видеть что получилось, а так же оставил и с укр но он в коментарии 
+	*/
+
+
+
+
+
+
+	return 0;
+}
