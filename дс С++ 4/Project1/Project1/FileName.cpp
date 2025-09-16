fb62cca9cbcab38a3618dfc5bfc5c06d25764564
notrakd <dmitrytroyt@gmail.com>
2025-09-16 19:26:11 +0300

    Add files via upload

---
+++ дс С++ 4/Project1/Project1/FileName.cpp
@@ -0,0 +1,29 @@
+﻿//Задание 1 
+
+#include <iostream>
+using namespace std;
+int main() {
+	
+	int a = 44;
+	int b = 544;
+	int c = a + b;
+
+	cout << "Price " << a << endl;
+	cout << "Na skladi " << b << endl;
+	cout << "Zagal produktov" << c;
+
+
+
+	
+
+	//Задание 2
+
+	const int N = 5;
+	const int F = 8;
+	cout << "Razom " << N + F << endl;
+	cout << "Na " << F - N << " " << "Menshe chem v 2";
+
+
+	return 0;
+
+}
