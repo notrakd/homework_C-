b2e133c8c0b3aca82719e5276175cbb8c35b4070
notrakd <dmitrytroyt@gmail.com>
2025-09-21 17:11:49 +0300

    Add files via upload

---
+++ дз с++ 5/Project1/Project1/FileName.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+
+ //Zadacha 1
+/*
+	int a;
+	int b;
+	cout << "Dovjina: ";
+	cin >> a;
+	cout << "Shirina: ";
+	cin >> b;
+	cout << "Perimetr " << 2 * (a + b) << endl;
+	cout << "Plosha: " << a * b;
+
+	*/
+
+	//zadacha 2
+
+	int a = 4000;
+	int b = 6000;
+	int c = 3000;
+	int d = 4500;
+	int e = 8000;
+	int f = 6000;
+
+	cout << "Zagal dohod za 6 month: " << a + b + c + d + e + f << endl;
+	cout << "Seredne arif: " <<  (a + b + c + d + e + f) / 6;
+
+
+
+
+}
