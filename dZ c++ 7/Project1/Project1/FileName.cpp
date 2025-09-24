09e9109235edba3e5db504306a4517a573a1007d
notrakd <dmitrytroyt@gmail.com>
2025-09-24 21:00:54 +0300

    Add files via upload

---
+++ dZ c++ 7/Project1/Project1/FileName.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+	//int n;
+	//cout << "Enter n";
+	//cin >> n;
+	//cout << "If y see the number 0, then the number is even" << endl;
+	//bool result = n % 2;
+	//cout << result;
+
+	int a;
+	cout << "Enter dni";
+	cin >> a;
+	int b = a / 7;
+	cout << "NEDELA " << b;
+	int c = a % 7;
+	cout << "dni " << c;
+
+}
