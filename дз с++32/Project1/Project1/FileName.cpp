e9bc595ca3d49639fac5059982359a792da83acc
notrakd <dmitrytroyt@gmail.com>
2025-10-07 12:50:43 +0300

    Add files via upload

---
+++ дз с++32/Project1/Project1/FileName.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	//int a;
+	//cout << "Enter trechznachne chislo";
+	//cin >> a;
+
+	//if (a < 100 || a > 999) {
+	//	cout << "ne trechznachne" << endl;
+	//}
+	//else
+	//{
+	//	cout << "trechznachne" << endl;
+	//}
+
+	//int b = a / 100;
+	///*cout << b << endl;*/
+	//int c = (a / 10) % 10;
+	////cout << c << endl;
+	//int x = a % 10;
+	////cout << x << endl;
+
+	//if (b == x) {
+	//	cout << a << endl << "PAlindrom";
+	//}
+	//else {
+	//	cout << a << endl << "Ne palindrom";
+	//}
+
+	int a;
+	cout << "Enter vik";
+	cin >> a;
+	if (a > 18) {
+		cout << "VI sovershenoletni";
+
+	}
+	else
+	{
+		cout << " vi nesovershenoletni";
+	}
+
+
+
+
+	return 0;
+}
