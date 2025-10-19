03f1a7a00cf1763274863c3c21fe32a952c7d36c
notrakd <dmitrytroyt@gmail.com>
2025-10-19 14:13:39 +0300

    Add files via upload

---
+++ dz c++324/Project1/Project1/2212.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //int a = 1;
+    //int b = -80;
+
+    //while (b <= 80) {
+    //    if (b % 2 != 0 && b % 7 == 0) {
+    //        a *= b;
+    //    }
+    //    b++;
+    //}
+
+    //cout << a << endl;
+
+
+    int a = 100;
+
+    while (a <= 200) {
+       if (a % 2 != 0) { 
+                cout << a << " ";
+            }
+            a++;
+        }
+
+        cout << endl;
+        return 0;
+    }
---
+++ dz c++324/Project1/Project1/FileName.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1;
+    int b = -80;
+
+    while (b <= 80) {
+        if (b % 2 != 0 && b % 7 == 0) { 
+            a *= i;
+        }
+        b++;
+    }
+
+    cout << a << endl;
+    return 0;
+}
